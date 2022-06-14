71fe5199f8a068f6c3522814e60c36209855cd75
James Wu <ichangjwu@outlook.com>
2022-06-14 19:52:27 +0900

    Add background

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import 'normalize.css';
+import './style.css';
+import Image from './background.jpg'
+
+//const myBackground = new Image()
+//myBackground.src = Background
+
+
+
+console.log("test")
